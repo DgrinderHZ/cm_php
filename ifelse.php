bf7f019d4747e7256b405dcf9d7c170c488566e8
DgrinderHZ <hassanzekkouri@gmail.com>
2022-11-11 17:09:13 +0100

    Ajouter switch

--- ifelse.php
+++ ifelse.php
@@ -41,6 +41,34 @@
 		echo "le nombre $un_nombre est nul." . RETOUR_A_LA_LIGNE;
 	}
 
+	// switch, jours de la semaine
+	$jour = 6;
+	switch ($jour) {
+		case 1:
+			echo "Lundi" . RETOUR_A_LA_LIGNE;
+			break;
+		case 2:
+			echo "Mardi" . RETOUR_A_LA_LIGNE;
+			break;
+		case 3:
+			echo "Mercredi" . RETOUR_A_LA_LIGNE;
+			break;
+		case 4:
+			echo "Jeudi" . RETOUR_A_LA_LIGNE;
+			break;
+		case 5:
+			echo "Vendredi" . RETOUR_A_LA_LIGNE;
+			break;
+		case 6:
+			echo "Samedi" . RETOUR_A_LA_LIGNE;
+			break;
+		case 7:
+			echo "Dimanche" . RETOUR_A_LA_LIGNE;
+			break;
+		
+		default:
+			echo "Numero unvalide.". RETOUR_A_LA_LIGNE;
+	}
 	?>
 
 </body>
