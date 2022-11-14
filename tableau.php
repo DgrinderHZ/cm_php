c977f3ae33cc47e0dea4b5158e9bba23423eb9a7
DgrinderHZ <hassanzekkouri@gmail.com>
2022-11-14 16:34:40 +0100

    Les tableaux fin seance 5

--- tableau.php
+++ tableau.php
@@ -22,7 +22,34 @@
         echo $les_notes[$i] . "<br>";
     }
 
+    echo "Modification des éléments <br> <hr>";
+    // Modification des éléments
+    $les_notes[3] = 20;
+    $les_notes[1] = $les_notes[0] + $les_notes[4];
+
+    // Affichage
+    for ($i=0; $i <= 4; $i++) { 
+        echo $les_notes[$i] . "<br>";
+    }
+
+    // syntaxe 2 de declaration d'un tableau php
+    $les_noms = ["Hassan", "Salah", "Salah2", "Salah3", "abdo", "mimon", "Ismail", "Soufian"];
+ 
     // FIN CODE PHP
     ?>
+
+    <br><hr>
+    <table>
+        <tr>
+            <th>Numero</th>
+            <th>Nom</th>
+        </tr>
+        <?php
+            for ($i=0; $i < count($les_noms); $i++) { 
+                echo "<tr><td>$i</td>";
+                echo "<td>$les_noms[$i]</td></tr>";
+            }
+        ?>
+    </table>
 </body>
 </html>
